8d88973a2090cc07507e0d832a2a7de1b34345d2
George Kokush <g.kokush5@gmail.com>
2024-07-02 14:47:13 +0300

    Add SoftmaxCosineCELoss

--- scripts/loss/__init__.py
+++ scripts/loss/__init__.py
@@ -1,9 +1,11 @@
 from scripts.loss.CELossWrapper import CELossWrapper as CELoss
 from scripts.loss.CELossWrapper import CosineCELossWrapper as CosineCELoss
 from scripts.loss.CELossWrapper import ExpCosineCELossWrapper as ExpCosineCELoss
+from scripts.loss.CELossWrapper import SoftmaxCosineCELossWrapper as SoftmaxCosineCELoss
 
 __all__ = [
     "CELoss",
     "CosineCELoss",
-    "ExpCosineCELoss"
+    "ExpCosineCELoss",
+    "SoftmaxCosineCELoss"
 ]
